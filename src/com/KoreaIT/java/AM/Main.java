1845e85698bb4644ad112f71d197297a7ded9881
SeoDongYeon1 <ehddus2655@gmail.com>
2023-03-10 17:49:13 +0900

    회원가입 기능 구현

--- src/com/KoreaIT/java/AM/Main.java
+++ src/com/KoreaIT/java/AM/Main.java
@@ -6,6 +6,7 @@ import java.util.Scanner;
 
 public class Main {
 	static List<Article> articles = new ArrayList<>();
+	static List<Member> members = new ArrayList<>();
 
 	public static void main(String[] args) {
 		System.out.println("==프로그램 시작==");
@@ -13,6 +14,7 @@ public class Main {
 		maketestData();
 
 		int lastArticleId = 3;
+		int lastMemberId = 0;
 
 		Scanner sc = new Scanner(System.in);
 
@@ -113,7 +115,7 @@ public class Main {
 					continue;
 				}
 
-			} 
+			}
 			else if (command.startsWith("article delete")) 
 			{
 				String[] commandBits = command.split(" ");
@@ -177,6 +179,64 @@ public class Main {
 					continue;
 				}
 			}
+			else if (command.equals("member join")) 
+			{	
+				int id = lastMemberId + 1;
+				String loginId = null;
+				String loginPw = null;
+				while(true) {
+					System.out.printf("아이디 : ");
+					loginId = sc.nextLine();
+					
+					if(isJoinableLoginId(loginId) == false) {
+						System.out.println("이미 사용중인 아이디입니다.");
+						continue;
+					}
+					else {
+						System.out.println("사용 가능한 아이디입니다.");
+						break;
+					}
+				}
+					
+				while(true) {
+					System.out.printf("비밀번호 : ");
+					loginPw = sc.nextLine();
+					System.out.printf("비밀번호 재확인: ");
+					String loginPw1 = sc.nextLine();
+					if(loginPw.equals(loginPw1)) {
+						System.out.println("비밀번호가 일치합니다.");
+						break;
+					}
+					else {
+						System.out.println("비밀번호가 일치하지 않습니다.");
+						continue;
+					}	
+				}
+					
+				System.out.printf("이름 : ");
+				String name = sc.nextLine();
+				
+				String regDate = Util.getNowDate();
+				
+				Member member = new Member(id, loginId, loginPw, name, regDate);
+				members.add(member);
+				
+				System.out.printf("%s님 회원가입 되었습니다.\n", name);
+				lastMemberId++;
+			}
+			else if (command.equals("member list")) 
+			{	
+				if(members.size()==0) {
+					System.out.println("회원이 존재하지 않습니다.");
+				}
+				else {
+					System.out.println("번호 / 이름        / 아이디     / 비밀번호     / 가입일자     ");
+					for(int i = members.size()-1; i >= 0; i--) {
+						Member member = members.get(i);
+						System.out.printf("%d   / %s         / %s     / %s     / %s     \n", member.id, member.name, member.loginId, member.loginPw, member.regDate);
+					}
+				}
+			}
 			else {
 				System.out.println("존재하지 않는 명령어입니다.");
 			}
@@ -186,7 +246,28 @@ public class Main {
 
 		sc.close();
 	}
+	
+	public static boolean isJoinableLoginId(String loginId) {
+		int index = getMemberIndexByLoginId(loginId);
+		
+		if(index == -1) {
+			return true;
+			}
+		
+		return false;
+		}
 
+	private static int getMemberIndexByLoginId(String loginId) {
+		int i = 0;
+		for (Member member : members) {
+			if (member.loginId.equals(loginId)) {
+				return i;
+			}
+			i++;		
+		}
+		return -1;
+	}
+	
 	private static int getArticleIndexById(int id) {
 		int i = 0;
 		for (Article article : articles) {
@@ -198,7 +279,7 @@ public class Main {
 	}
 
 	public static Article getArticleById(int id) {
-// ver1
+		// ver1
 //		for (int i = 0; i < articles.size(); i++) {  
 //			Article article = articles.get(i);
 //			if (article.id == id) {
@@ -207,14 +288,14 @@ public class Main {
 //			
 //		}
 		
-// ver2		
+		// ver2
 //		for (Article article : articles) {
 //			if (article.id == id) {
 //				return article;
 //			}
 //		}
 		
-// ver3		
+		// ver3
 		int index = getArticleIndexById(id);
 		
 		if(index != -1) {
@@ -258,18 +339,16 @@ class Article {
 
 class Member {
 	int id;
-	String name;
 	String loginId;
 	String loginPw;
+	String name;
 	String regDate;
-	String nickname;
 	
-	Member(int id, String loginId, String loginPw, String name, String nickname, String regDate) {
+	Member(int id, String loginId, String loginPw, String name, String regDate) {
 		this.id = id;
 		this.loginId = loginId;
 		this.loginPw = loginPw;
 		this.name = name;
-		this.nickname = nickname;
 		this.regDate = regDate;
 	}
 }
